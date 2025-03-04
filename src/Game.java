46ac17dba05a5c994aceb2e1d3a38ab4ac36fd3a
Rhouma Yassine <yassine.rhouma@icloud.com>
2025-03-04 11:45:18 +0100

    Implement basic game loop

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
     private boolean isRunning = false;
+    private List<Element> elements = new ArrayList<>();
     
     public Game() {
         // Constructor
@@ -7,11 +11,31 @@ public class Game {
     
     public void start() {
         isRunning = true;
-        System.out.println("Game started");
+        gameLoop();
     }
     
     public void stop() {
         isRunning = false;
         System.out.println("Game stopped");
+    }
+    
+    private void gameLoop() {
+        while (isRunning) {
+            // Update all game elements
+            for (Element element : elements) {
+                element.update();
+            }
+            
+            // Sleep to control game speed
+            try {
+                Thread.sleep(16); // ~60 FPS
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    public void addElement(Element element) {
+        elements.add(element);
     }
 }
