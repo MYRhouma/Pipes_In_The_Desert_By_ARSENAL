0b2ac4a010cbe2b9377fcb12537fab9c825046d7
Rhouma Yassine <yassine.rhouma@icloud.com>
2025-03-19 14:33:42 +0100

    Connect MapGUI to Game class

--- src/Game.java
+++ src/Game.java
@@ -4,13 +4,27 @@ import java.util.List;
 public class Game {
     private boolean isRunning = false;
     private List<Element> elements = new ArrayList<>();
+    private List<Pipe> pipes = new ArrayList<>();
+    private List<Pump> pumps = new ArrayList<>();
+    private List<Cistern> cisterns = new ArrayList<>();
+    private List<Spring> springs = new ArrayList<>();
+    private Player[] players;
+    private int currentPlayerIndex = 0;
+    
+    public MapGUI mapGUI;
     
     public Game() {
         // Constructor
     }
     
+    public void initMap() {
+        mapGUI = new MapGUI(this);
+        mapGUI.setVisible(true);
+    }
+    
     public void start() {
         isRunning = true;
+        initMap();
         gameLoop();
     }
     
@@ -26,6 +40,11 @@ public class Game {
                 element.update();
             }
             
+            // Update map GUI
+            if (mapGUI != null) {
+                mapGUI.updateMap();
+            }
+            
             // Sleep to control game speed
             try {
                 Thread.sleep(16); // ~60 FPS
@@ -37,5 +56,25 @@ public class Game {
     
     public void addElement(Element element) {
         elements.add(element);
+    }
+    
+    public void addPipe(Pipe pipe) {
+        elements.add(pipe);
+        pipes.add(pipe);
+    }
+    
+    public void addPump(Pump pump) {
+        elements.add(pump);
+        pumps.add(pump);
+    }
+    
+    public void addCistern(Cistern cistern) {
+        elements.add(cistern);
+        cisterns.add(cistern);
+    }
+    
+    public void addSpring(Spring spring) {
+        elements.add(spring);
+        springs.add(spring);
     }
 }
